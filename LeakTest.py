52847a8aa213beaf8c7be38b692db1631b5d095c
Akshay R.Shenoy <64606052+ItsAkshayR@users.noreply.github.com>
2021-10-23 19:53:46 +0530

    Basic Final Build Is Done
    Made It Working With All Functions In One PassMan.py Which Main File.

--- LeakTest.py
+++ LeakTest.py
@@ -1,9 +1,8 @@
 import pwnedpasswords
 
-pwd = input("Please Enter Your Password To Check Wheather Its Leaked Or Not")
-pwd_con = pwnedpasswords.check(pwd)
-
-if(pwd_con == 0):
-    print("Your Password Is safe To Use")
-else:
-    print("Your Password Is Been Leaked On Web")
+def check(pwd):
+    pwd_con = pwnedpasswords.check(pwd)
+    if(pwd_con == 0):
+        return True
+    else:
+        return False
